7b0d60da18f6675c27e8421a1062bce0fcd41c38
Jason Huynh <jasonn0203@gmail.com>
2023-06-05 22:59:53 +0700

    Test

--- AirBnb/Controllers/HomeController.cs
+++ AirBnb/Controllers/HomeController.cs
@@ -22,7 +22,7 @@ namespace AirBnb.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Your contacttttttt page.";
 
             return View();
         }
