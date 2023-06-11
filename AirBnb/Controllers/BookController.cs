74edb967d5db4c4caf10c3193e9357c747468463
Jason Huynh <jasonn0203@gmail.com>
2023-06-11 22:17:53 +0700

    Date Picker, Payment

---
+++ AirBnb/Controllers/BookController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AirBnb.Controllers
+{
+    public class BookController : Controller
+    {
+        // GET: Book
+        public ActionResult Stay()
+        {
+            return View();
+        }
+    }
+}
