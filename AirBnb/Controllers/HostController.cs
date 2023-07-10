27488bfc3c6d78fedded3a64570d5401a59e6880
Jason Huynh <jasonn0203@gmail.com>
2023-07-10 13:22:31 +0700

    update 10/7

---
+++ AirBnb/Controllers/HostController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AirBnb.Controllers
+{
+    public class HostController : Controller
+    {
+        // GET: Host
+        public ActionResult HostSignUp()
+        {
+            return View();
+        }
+
+        public ActionResult HostSignIn()
+        {
+            return View();
+        }
+
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+    }
+}
