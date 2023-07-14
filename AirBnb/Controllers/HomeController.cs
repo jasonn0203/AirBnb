24bf38d284830310ed0fd5693623b489e4ff3a55
Jason Huynh <jasonn0203@gmail.com>
2023-07-14 22:24:04 +0700

    Kết nối DB, update 14/7

--- AirBnb/Controllers/HomeController.cs
+++ AirBnb/Controllers/HomeController.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using AirBnb.Models;
 
 namespace AirBnb.Controllers
 {
     public class HomeController : Controller
+
     {
+
+        //Get database
+        AirbnbEntities db = new AirbnbEntities();
+
+
         // GET: Home
         public ActionResult Index()
         {
@@ -28,5 +35,13 @@ namespace AirBnb.Controllers
         {
             return View();
         }
+
+        public ActionResult GetCategories()
+        {
+            var categoriesList = db.DanhMucPhongs.ToList();
+            return PartialView(categoriesList);
+        }
+
+
     }
 }
